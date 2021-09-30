e9d261eee0b0528028bf6ce4845c363c4cc2de95
bobo <bobo511326@gmail.com>
2021-09-30 17:26:43 +0800

    version 2021/09/30 17:26

--- GUI_main2.py
+++ GUI_main2.py
@@ -5,12 +5,6 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 # ====== <parameter> ====== #
-align_mode = 'nswe'
-pad = 5
-# widget size
-div_height = 100
-div_width = 100
-# custom value
 SWEETNESS_ENABLE = False
 SWEETNESS = 'Normal Sweet'
 ACIDITY_ENABLE = False
@@ -24,28 +18,31 @@ class Tkwindow():
         # initialize window name, size
         self.window = tk.Tk()
         self.window.title('Burette Motor')
-        self.window.geometry('600x300')
+        self.window.geometry('450x150')
 
         # setting frame of the widget
-        div1 = tk.Frame(self.window, width=div_width*2, height=div_height, bg='blue')
-        div2 = tk.Frame(self.window, width=div_width, height=div_height, bg='orange')
-        div3 = tk.Frame(self.window, width=div_width, height=div_height, bg='green')
+        div1 = tk.Frame(self.window, width=450, height=100, bg='blue')
+        div2 = tk.Frame(self.window, width=225, height=50, bg='green')
+        div3 = tk.Frame(self.window, width=225, height=50, bg='red')
         # setting widget position = (column, row)
-        div1.grid(column=0, row=0, padx=pad, pady=pad, columnspan=2, sticky=align_mode)
-        div2.grid(column=0, row=1, padx=pad, pady=pad, sticky=align_mode)
-        div3.grid(column=1, row=1, padx=pad, pady=pad, sticky=align_mode)
+        div1.grid(column=0, row=0, columnspan=2, padx=5)
+        div2.grid(column=0, row=1, padx=50, pady=5)
+        div3.grid(column=1, row=1, padx=50, pady=5)
 
-        self.define_layout(self.window, 2, 2)
+        self.define_layout(self.window, cols=2, rows=2)
         self.define_layout([div1, div2, div3])
+        
+        lb1 = tk.Label(div1, text="Which mode do you want to choose?\nAuto or Custom mode.", font=('Arial', 18))
+        lb1.grid()
+        
+        bt_1 = tk.Button(div2, text='Auto Mode', font=('Arial', 14), bg='gray', fg='white', command=self.autoMode)
+        bt_1.grid(sticky='e')
+        bt_2 = tk.Button(div3, text='Custom Mode', font=('Arial', 14), bg='gray', fg='white', command=self.customMode)
+        bt_2.grid(sticky='w')
 
-        self.create_label(div1, "Which mode do you want to choose?\nAuto or Custom mode.")
-
-        self.create_button(div2, 'Auto Mode', self.autoMode)
-        self.create_button(div3, 'Custom Mode', self.customMode)
-
-        self.define_layout(self.window, 2, 2)
+        self.define_layout(self.window, cols=2, rows=2)
         self.define_layout([div1, div2, div3])
-
+        
         self.window.mainloop()
 
     # quit window
@@ -72,111 +69,100 @@ class Tkwindow():
         #messagebox.showerror('MessageBox Error', 'There is an error.')
         #messagebox.askquestion('MessageBox Question', 'Confirm ?')
 
-    # create label
-    def create_label(self, lb_where, lb_txt, lb_width=100, lb_height=2, lb_bg='white', lb_fg='black', lb_font=('Arial', 22)):
-        label = tk.Label(lb_where, text=lb_txt, bg=lb_bg, fg=lb_fg, font=lb_font, width=lb_width, height=lb_height)
-        label.grid(column=0, row=0, sticky=align_mode)
-
-    # create button
-    def create_button(self, bt_where, bt_txt, bt_command=msg_box, bt_bg='blue', bt_fg='white', p_bg='white', p_fg='blue', bt_width=50, bt_height=4):
-        bt_1 = tk.Button(bt_where, text=bt_txt, bg=bt_bg, fg=bt_fg, font=('Arial', 22), command=bt_command)
-        bt_1['width'] = bt_width
-        bt_1['height'] = bt_height
-        # button background color when the button is press
-        bt_1['activebackground'] = p_bg
-        # button text color when the button is press
-        bt_1['activeforeground'] = p_fg
-        bt_1.grid(column=0, row=0, sticky=align_mode)
-
-    # confirm sweet choose
-    def set_sweet(event):
-        SWEETNESS_ENABLE = True
-        ACIDITY_ENABLE = False
-
-    # adjust sweetness
-    def sweet(self):
-        sweetChoose = tk.Tk()
-        sweetChoose.title('Burette Motor <Adjust Sweeetness>')
-        sweetChoose.geometry('360x100')
-        # setting frame of the widget
-        sweet_div1 = tk.Frame(sweetChoose, width=350, height=20, bg='blue')
-        sweet_div2 = tk.Frame(sweetChoose, width=350, height=50, bg='green')
-
-        # setting widget position = (column, row)
-        sweet_div1.grid(column=0, row=0, padx=pad, pady=pad, sticky=align_mode)
-        sweet_div2.grid(column=0, row=1, padx=pad, pady=pad, sticky=align_mode)
-        self.define_layout(sweetChoose, cols=1, rows=2)
-        self.define_layout([sweet_div1, sweet_div2])
-
-        self.create_label(sweet_div1, 'Adjust how sweet you want.', 100, 2, 'white', 'black', ('Arial', 14))
-
-        # sweetness menu options
-        opt = ttk.Combobox(sweet_div2, values=['No Sweet', 'Quarter Sweet', 'Half Sweet', 'Less Sweet', 'Normal Sweet', 'Very Sweet', 'Super Sweet'])
-        opt.current(4)
-        opt.bind('<<ComboboxSelected>>', self.set_sweet)
-        opt.grid(column=0, row=0, sticky=align_mode)
-
-        self.define_layout(sweetChoose, cols=1, rows=2)
-        self.define_layout([sweet_div1, sweet_div2])
-
-        sweetChoose.mainloop()
-
     def autoMode(self):
         messagebox.showinfo('Burette Motor <info>', 'You choose the auto mode, the wine is mixing...')
 
     def customMode(self):
         custom_window = Custom()
         
-
 # custom mode window
 class Custom(Tkwindow):
     # custom mode window
     def __init__(self):
         self.window = tk.Tk()
         self.window.title('Burette Motor <Custom Mode>')
-        self.window.geometry('400x230')
+        self.window.geometry('350x90')
 
         # setting frame of the widget
-        cMode_div1 = tk.Frame(self.window, width=390, height=50, bg='blue')
-        cMode_div2 = tk.Frame(self.window, width=390, height=40, bg='orange')
-        cMode_div3 = tk.Frame(self.window, width=390, height=40, bg='green')
-        cMode_div4 = tk.Frame(self.window, width=140, height=40, bg='white')
-        cMode_div5 = tk.Frame(self.window, width=70, height=40, bg='red')
-        cMode_div6 = tk.Frame(self.window, width=140, height=40, bg='white')
+        cMode_div1 = tk.Frame(self.window, width=350, height=50, bg='blue')
+        cMode_div2 = tk.Frame(self.window, width=350, height=40, bg='orange')
 
-        # setting widget position = (column, row)
-        cMode_div1.grid(column=0, row=0, columnspan=3, padx=pad, pady=pad, sticky=align_mode)
-        cMode_div2.grid(column=0, row=1, columnspan=3, padx=pad, pady=pad, sticky=align_mode)
-        cMode_div3.grid(column=0, row=2, columnspan=3, padx=pad, pady=pad, sticky=align_mode)
-        cMode_div4.grid(column=0, row=3, padx=pad, pady=pad, sticky=align_mode)
-        cMode_div5.grid(column=1, row=3, padx=pad, pady=pad, sticky=align_mode)
-        cMode_div6.grid(column=2, row=3, padx=pad, pady=pad, sticky=align_mode)
-
-        self.define_layout(self.window, cols=3, rows=4)
-        self.define_layout([cMode_div1, cMode_div2, cMode_div3, cMode_div4, cMode_div5, cMode_div6])
-        
-        # create_label(lb_where, lb_txt, lb_width=100, lb_height=2, lb_bg='white', lb_fg='black', lb_font=('Arial', 22))
-        self.create_label(cMode_div1, 'Choose one parameter to adjust.', 100, 2, 'white', 'black', ('Arial', 14))
-        
-        # seleced is the choise that user make
-        selected = tk.IntVar()
-        # initial radio button
-        selected.set = 0
-        
-        # radiobutton
-        bt1 = tk.Radiobutton(cMode_div2, text='Sweetness', variable=selected, value=1, command=self.msg_box)
-        bt2 = tk.Radiobutton(cMode_div3, text='Acidity', variable=selected, value=2, command=self.msg_box)
-
-        bt1.grid(column=0, row=0, sticky=align_mode)
-        bt2.grid(column=0, row=0, sticky=align_mode)
-
-        # confirm button
-        self.create_button(cMode_div5, 'Confirm', self.quit, 'blue', 'white', 'white', 'blue', bt_width=8, bt_height=1)
+        cMode_div1.grid(column=0, row=0, padx=5, pady=5)
+        cMode_div2.grid(column=0, row=1, padx=5)
 
         self.define_layout(self.window, cols=1, rows=3)
-        self.define_layout([cMode_div1, cMode_div2, cMode_div3, cMode_div4, cMode_div5, cMode_div6])
+        self.define_layout([cMode_div1, cMode_div2])
+        
+        lb1 = tk.Label(cMode_div1, text='Choose one parameter to adjust.', font=('Arial', 14))
+        lb1.grid()
+        
+        # seleced is the choise that user make
+        selected = tk.StringVar()
+        # initial radio button
+        selected.set = 'Sweet'
+        
+        # radiobutton
+        bt1 = tk.Radiobutton(cMode_div2, text='Sweetness', variable=selected, value='Sweet', font=('Arial', 12), command=self.sweetAdjust)
+        bt1.grid(column=0, row=0, sticky='w')
+        bt2 = tk.Radiobutton(cMode_div2, text='Acidity', variable=selected, value='Acid', font=('Arial', 12), command=self.msg_box)  
+        bt2.grid(column=1, row=0, sticky='e')
+
+        self.define_layout(self.window, cols=1, rows=3)
+        self.define_layout([cMode_div1, cMode_div2])
         
         self.window.mainloop()
+
+    # adjust sweetness
+    def sweetAdjust(self):
+        self.quit()
+        sweetAdustWindow = Sweet()
+        
+# adjust sweetness window 
+class Sweet(Custom):
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title('Burette Motor <Adjust Sweeetness>')
+        self.window.geometry('350x70')
+        # setting frame of the widget
+        sweet_div1 = tk.Frame(self.window, width=350, height=40, bg='blue')
+        sweet_div2 = tk.Frame(self.window, width=230, height=30, bg='green')
+        sweet_div3 = tk.Frame(self.window, width=120, height=30, bg='red')
+
+        # setting widget position = (column, row)
+        sweet_div1.grid(column=0, row=0, columnspan=2, padx=5)
+        sweet_div2.grid(column=0, row=1, padx=10, pady=5)
+        sweet_div3.grid(column=1, row=1, padx=10, pady=5)
+        
+        self.define_layout(self.window, cols=2, rows=2)
+        self.define_layout([sweet_div1, sweet_div2, sweet_div3])
+        
+        lb1 = tk.Label(sweet_div1, text='Adjust how sweet you want.', font=('Arial', 14))
+        lb1.grid()
+        
+        # sweetness menu options (default as normal sweet)
+        self.opt = ttk.Combobox(sweet_div2, state='readonly')
+        self.opt['values'] = ['No Sweet', 'Quarter Sweet', 'Half Sweet', 'Less Sweet', 'Normal Sweet', 'Very Sweet', 'Super Sweet']
+        self.opt.current(4)
+        self.opt.grid(sticky='e')
+
+        bt_1 = tk.Button(sweet_div3, text='Confirm', font=('Arial', 10), bg='gray', fg='white', command=self.set_sweet)
+        bt_1.grid(sticky='w')
+        
+        self.define_layout(self.window, cols=2, rows=2)
+        self.define_layout([sweet_div1, sweet_div2, sweet_div3])
+        
+        self.window.mainloop()
+
+        # confirm sweet choose
+    def set_sweet(self):
+        global SWEETNESS, SWEETNESS_ENABLE, ACIDITY, ACIDITY_ENABLE
+        SWEETNESS_ENABLE = 'True'
+        ACIDITY_ENABLE = 'False'
+        print('set <SWEETNESS_ENABLE> ', SWEETNESS_ENABLE)
+        SWEETNESS = self.opt.get()
+        print('set <SWEETNESS> ', SWEETNESS)
+        self.quit()
+
 
 root = Tkwindow()
 
