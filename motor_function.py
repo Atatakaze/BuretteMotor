6e76855466a746077edc9b97263e9ee3be116ad3
bobo <bobo511326@gmail.com>
2021-10-04 20:07:36 +0800

    version 2021/10/04 20:07

--- motor_function.py
+++ motor_function.py
@@ -15,9 +15,9 @@ import sys
 # calculating the duration needed for the target volume
 def calVolume(motor, volume):
     if motor == 'MOTOR1':
-        duration = ( volume - 5.4 ) / 6.075
+        duration = ( volume - 5.4 ) / 6.08
     elif motor == 'MOTOR2':
-        duration = ( volume - 3.8667 ) / 0.422
+        duration = ( volume - 3.87 ) / 0.42
     return duration
 
 # mixing two wine
